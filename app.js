70bc962a219c2e74df12bdedb516f03db8b31d89
Liza0806 <126647343+Liza0806@users.noreply.github.com>
2023-11-30 08:55:40 +0300

    d

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const contactsRouter = require('./routes/api/contacts')
 
 const app = express()
 
+
+const mongoose = require('mongoose')
+// const Mongo = 'mongodb+srv://Liza:asX11J89YYmgZCbI@cluster0.v1ocvyh.mongodb.net/contacts-base?retryWrites=true&w=majority'
+const {DB_HOST} = require('./config')
+mongoose.connect(DB_HOST)
+.then(()=> console.log("Database connect success"))
+.catch(error=> console.log(error.message))
+
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger))
---
+++ config.js
@@ -0,0 +1,5 @@
+const DB_HOST = 'mongodb+srv://Liza:asX11J89YYmgZCbI@cluster0.v1ocvyh.mongodb.net/contacts-base?retryWrites=true&w=majority' // process.env
+
+module.exports = {
+    DB_HOST,
+}
