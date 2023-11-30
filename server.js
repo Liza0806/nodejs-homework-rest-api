c176b647fd79979a3f482445b7325a46c357f45f
Liza0806 <126647343+Liza0806@users.noreply.github.com>
2023-11-30 09:07:46 +0300

    s

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const app = require('./app')
+const dotenv = require('dotenv')
+dotenv.config();
 
-app.listen(3002, () => {
-  console.log("Server running. Use our API on port: 3002")
-})
+const mongoose = require('mongoose')
+// const Mongo = 'mongodb+srv://Liza:asX11J89YYmgZCbI@cluster0.v1ocvyh.mongodb.net/contacts-base?retryWrites=true&w=majority'
+const {DB_HOST} = process.env
+mongoose.connect(DB_HOST)
+.then(()=> 
+{
+  app.listen(3002, () => {
+    console.log("Server running. Use our API on port: 3002")
+  })
+  console.log("Database connect success")})
+.catch(error=> console.log(error.message))
+// app.listen(3002, () => {
+//   console.log("Server running. Use our API on port: 3002")
+// })
